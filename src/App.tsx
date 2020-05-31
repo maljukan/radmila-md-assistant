dca0984775c262b5911e9f86447acad6594d55e2
n.maljukan <n.maljukan@levi9.com>
2020-05-31 02:49:31 +0200

    Integrate `Tailwind`

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Radmila</h1>
+        <svg width="108px" height="128px" viewBox="0 0 54 64" version="1.1" xmlns="http://www.w3.org/2000/svg"
+             xmlnsXlink="http://www.w3.org/1999/xlink">
+          <defs></defs>
+          <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" type="MSPage">
+            <path className="beat-loader"
+                  d="M0.5,38.5 L16,38.5 L19,25.5 L24.5,57.5 L31.5,7.5 L37.5,46.5 L43,38.5 L53.5,38.5" id="Path-2"
+                  strokeWidth="2" type="MSShapeGroup">
+            </path>
+          </g>
+        </svg>
+        <small>doctor's assistant</small>
       </header>
     </div>
   );
